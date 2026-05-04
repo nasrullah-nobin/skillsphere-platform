3d66137cbd9bd960c9b69ca0bddfd501c11fbc4e
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-05-04 11:39:39 +0600

    jfjfjf

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -5,8 +5,9 @@ import Link from "next/link";
 import { Button } from "@heroui/react";
 import { BiCategory } from "react-icons/bi";
 import CourseSearch from "@/Components/CourseSearch";
-
+export const dynamic = 'force-dynamic'
 const AllCourses = async ({searchParams}) => {
+  
   const courses = await getCourses();
   
     const params = await searchParams;
